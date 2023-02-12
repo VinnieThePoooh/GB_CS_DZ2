1c2bf4ad52d201b6212dea7d1a317cce624b11e9
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-02-12 19:42:09 +0300

    solved task1

---
+++ folder1/task1.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, 
+// которая принимает на вход трёхзначное число и на выходе показывает
+//  вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    int result1  = number / 10;
+    int result2 = result1 % 10;
+    System.Console.WriteLine(result2);
+}
+else
+{
+    System.Console.WriteLine("Пожалуйста, введите ТРЕХЗНАЧНОЕ число");
+}
