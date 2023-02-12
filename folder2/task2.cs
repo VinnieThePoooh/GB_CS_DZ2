9eaf03e0723d5d89e98a3d4296ee9fd2bdb7639c
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-02-12 18:31:17 +0300

    сохранил условие

---
+++ folder2/task2.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine("сейчас придумаем");
+}
