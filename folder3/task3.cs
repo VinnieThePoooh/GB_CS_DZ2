d35edd7550ea62432b270b3672364c4d4b5b4bb9
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-02-12 19:36:01 +0300

    solved task3

---
+++ folder3/task3.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите цифру, обозначающую день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<1 || number>7)
+{
+    System.Console.WriteLine("Введенное значение не обозначает день недели");
+}
+else
+{
+    if ( number == 6 || number == 7)
+    {
+        System.Console.WriteLine("Выходной");
+    }
+    else
+    {
+        System.Console.WriteLine("Будний");
+    }
+}
